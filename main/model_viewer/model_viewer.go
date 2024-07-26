ec33eced744b43be9c142151841343886840e33b
Klear <kingklear@gmail.com>
2024-07-26 21:42:28 +0200

    Upgraded file formats.

--- main/model_viewer/model_viewer.go
+++ main/model_viewer/model_viewer.go
@@ -1,82 +1,31 @@
 components {
   id: "model_viewer"
   component: "/main/model_viewer/model_viewer.script"
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
 }
 components {
   id: "model_viewer1"
   component: "/main/model_viewer/model_viewer.gui"
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
 }
 embedded_components {
   id: "create_mesh"
   type: "factory"
   data: "prototype: \"/main/model_viewer/mesh.go\"\n"
-  "load_dynamically: false\n"
-  "dynamic_prototype: false\n"
   ""
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
 }
 embedded_components {
   id: "create_transform"
   type: "factory"
   data: "prototype: \"/main/model_viewer/transform.go\"\n"
-  "load_dynamically: false\n"
-  "dynamic_prototype: false\n"
   ""
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
 }
 embedded_components {
   id: "background"
   type: "sprite"
   data: "default_animation: \"white_pixel\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
-  "blend_mode: BLEND_MODE_ALPHA\n"
   "size {\n"
   "  x: 400.0\n"
   "  y: 507.0\n"
-  "  z: 0.0\n"
-  "  w: 0.0\n"
   "}\n"
   "size_mode: SIZE_MODE_MANUAL\n"
   "textures {\n"
@@ -87,12 +36,5 @@ embedded_components {
   position {
     x: 634.0
     y: 293.5
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
   }
 }
